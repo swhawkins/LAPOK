acdeb5d74884d7682db605c57895875a0aca18b4
swhawkins <swhawkins@gmail.com>
2025-05-10 18:14:11 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -564,6 +564,7 @@ function OklahomaLAPApp() {
                         <option value="American Indian or Alaska Native">American Indian or Alaska Native</option>
                         <option value="Asian">Asian</option>
                         <option value="Black or African American">Black or African American</option>
+                        <option value="Hispanic">Hispanic</option>
                         <option value="Native Hawaiian or Other Pacific Islander">Native Hawaiian or Other Pacific Islander</option>
                         <option value="White">White</option>
                         <option value="Two or More Races">Two or More Races</option>
@@ -677,6 +678,7 @@ function OklahomaLAPApp() {
                         <option value="American Indian or Alaska Native">American Indian or Alaska Native</option>
                         <option value="Asian">Asian</option>
                         <option value="Black or African American">Black or African American</option>
+                        <option value="Hispanic">Hispanic</option>
                         <option value="Native Hawaiian or Other Pacific Islander">Native Hawaiian or Other Pacific Islander</option>
                         <option value="White">White</option>
                         <option value="Two or More Races">Two or More Races</option>
